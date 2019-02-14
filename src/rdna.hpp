efb4c9565da242b80512710bdc2c47df24601e85
frescante <frescante@users.noreply.github.com>
2019-02-14 16:01:03 -0400

    i done forreal

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -5,13 +5,20 @@ using namespace std;
 
 string randDNA(int seed, string bases, int x)
 {	
-	mt19937 eng1(seed);
+	mt19937 eng(seed);
 	string DNA;
+	
+	if (bases.size() == 0)
+	{
+		DNA= "";
+		return DNA;
+	}
+	
 	uniform_int_distribution<>un(0, bases.size()-1);
 	
 	for(int i=0 ;  i < x ;  i++) 
 	{ 
-		DNA+= bases[un(eng1)];
+		DNA+= bases[un(eng)];
 	}
 	return DNA; 
 }
